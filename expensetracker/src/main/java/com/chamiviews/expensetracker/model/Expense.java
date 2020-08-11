17bc27cd06d640720225bb13546bed2145221a45
chamod_lab <chamodlakmal97@gmail.com>
2020-08-11 10:42:29 +0530

    packages created

---
+++ expensetracker/src/main/java/com/chamiviews/expensetracker/model/Expense.java
@@ -0,0 +1,11 @@
+package com.chamiviews.expensetracker.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="tbl_expenses")
+public class Expense {
+	
+
+}
