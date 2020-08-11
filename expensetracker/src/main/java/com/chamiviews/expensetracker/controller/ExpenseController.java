d628786107290eb2439a2769465d4afad4717ecf
chamod_lab <chamodlakmal97@gmail.com>
2020-08-11 16:34:22 +0530

    get mapping added

---
+++ expensetracker/src/main/java/com/chamiviews/expensetracker/controller/ExpenseController.java
@@ -0,0 +1,28 @@
+package com.chamiviews.expensetracker.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.chamiviews.expensetracker.model.Expense;
+import com.chamiviews.expensetracker.service.ExpenseService;
+
+@RestController
+@RequestMapping("/api/v1")
+public class ExpenseController {
+	
+	@Autowired
+	ExpenseService expenseService;
+	
+	@GetMapping("/expenses")
+	public ResponseEntity<List<Expense>> get(){
+		 List<Expense> expenses= expenseService.findAll();
+		 return new ResponseEntity<List<Expense>>(expenses, HttpStatus.OK);
+	}
+
+}
