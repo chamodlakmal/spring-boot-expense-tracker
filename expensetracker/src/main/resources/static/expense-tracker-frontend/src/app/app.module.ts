cca8ccd5ab9d91d9259c87d0602c8981e4b39d0c
chamod_lab <chamodlakmal97@gmail.com>
2020-08-11 17:14:36 +0530

    angular updated

--- expensetracker/src/main/resources/static/expense-tracker-frontend/src/app/app.module.ts
+++ expensetracker/src/main/resources/static/expense-tracker-frontend/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListExpenseComponent } from './components/list-expense/list-expense.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListExpenseComponent
   ],
   imports: [
     BrowserModule,
