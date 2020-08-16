13f2f2f50b10263a85e34e3cfa82feedf5fb9fea
chamod_lab <chamodlakmal97@gmail.com>
2020-08-16 20:38:30 +0530

    final

--- expensetracker/src/main/resources/static/expense-tracker-frontend/src/app/components/add-expense/add-expense.component.ts
+++ expensetracker/src/main/resources/static/expense-tracker-frontend/src/app/components/add-expense/add-expense.component.ts
@@ -9,26 +9,26 @@ import { ExpenseService } from 'src/app/services/expense.service';
   styleUrls: ['./add-expense.component.css']
 })
 export class AddExpenseComponent implements OnInit {
-  expense:Expense=new Expense();
-  constructor(private _expenseService:ExpenseService,private _router:Router,
-    private _activatedRoute:ActivatedRoute) { }
+  expense: Expense = new Expense();
+  constructor(private _expenseService: ExpenseService, private _router: Router,
+    private _activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
-    const isIdPresent=this._activatedRoute.snapshot.paramMap.has('id')
-    if(isIdPresent){
-      this._expenseService.getExpense(+this._activatedRoute.snapshot.paramMap.get('id')).subscribe(data=>this.expense=data)
+    const isIdPresent = this._activatedRoute.snapshot.paramMap.has('id')
+    if (isIdPresent) {
+      this._expenseService.getExpense(+this._activatedRoute.snapshot.paramMap.get('id')).subscribe(data => this.expense = data)
     }
   }
 
-  saveExpense(){
-    this._expenseService.saveExpense(this.expense).subscribe(data=>{
-      console.log('response',data);
+  saveExpense() {
+    this._expenseService.saveExpense(this.expense).subscribe(data => {
+      console.log('response', data);
       this._router.navigateByUrl('/expenses');
     });
   }
 
-  deleteexpense(id:number){
-    this._expenseService.deleteExpense(id).subscribe(data=>{
+  deleteexpense(id: number) {
+    this._expenseService.deleteExpense(id).subscribe(data => {
       console.log('deleted response ', data)
       this._router.navigateByUrl('/expenses')
     })
--- expensetracker/src/main/resources/static/expense-tracker-frontend/src/app/components/list-expense/list-expense.component.ts
+++ expensetracker/src/main/resources/static/expense-tracker-frontend/src/app/components/list-expense/list-expense.component.ts
@@ -1,40 +1,40 @@
 import { Component, OnInit } from '@angular/core';
 import { Expense } from 'src/app/models/expense';
-import {ExpenseService} from '../../services/expense.service';
+import { ExpenseService } from '../../services/expense.service';
 @Component({
   selector: 'app-list-expense',
   templateUrl: './list-expense.component.html',
   styleUrls: ['./list-expense.component.css']
 })
 export class ListExpenseComponent implements OnInit {
-  expenses:Expense[];
+  expenses: Expense[];
 
-  filters={
-    keyword:'',
-    sortBy:'Name'
+  filters = {
+    keyword: '',
+    sortBy: 'Name'
 
   }
 
-  constructor(private _expenseService: ExpenseService ,) { }
+  constructor(private _expenseService: ExpenseService,) { }
 
   ngOnInit(): void {
     this.listExpenses()
   }
 
   listExpenses(): void {
-    this._expenseService.getExpenses().subscribe(data=>this.expenses=this.filterExpenses(data))
+    this._expenseService.getExpenses().subscribe(data => this.expenses = this.filterExpenses(data))
   }
 
-  filterExpenses(data:Expense[]){
-    return data.filter((e)=>{
+  filterExpenses(data: Expense[]) {
+    return data.filter((e) => {
       return e.expense.toLowerCase().includes(this.filters.keyword.toLowerCase())
-    }).sort((a,b)=>{
-      if(this.filters.sortBy==='Name'){
-        return a.expense.toLowerCase()<b.expense.toLowerCase()?-1:1;
-      }else if(this.filters.sortBy==='Amount'){
-        a.amount<b.amount?-1:1;
+    }).sort((a, b) => {
+      if (this.filters.sortBy === 'Name') {
+        return a.expense.toLowerCase() < b.expense.toLowerCase() ? -1 : 1;
+      } else if (this.filters.sortBy === 'Amount') {
+        a.amount < b.amount ? -1 : 1;
       }
     })
 
-}
+  }
 }
