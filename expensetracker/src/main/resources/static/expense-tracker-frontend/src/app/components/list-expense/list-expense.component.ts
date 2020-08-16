9f8959a8676c6b3dacfe855ec125b3ceded4d69e
chamod_lab <chamodlakmal97@gmail.com>
2020-08-16 19:41:45 +0530

    fllter added

--- expensetracker/src/main/resources/static/expense-tracker-frontend/src/app/components/list-expense/list-expense.component.ts
+++ expensetracker/src/main/resources/static/expense-tracker-frontend/src/app/components/list-expense/list-expense.component.ts
@@ -9,12 +9,25 @@ import {ExpenseService} from '../../services/expense.service';
 export class ListExpenseComponent implements OnInit {
   expenses:Expense[];
 
+  filters={
+    keyword:'',
+
+  }
+
   constructor(private _expenseService: ExpenseService ,) { }
 
   ngOnInit(): void {
     this._expenseService.getExpenses().subscribe(data=>this.expenses=data)
   }
 
-  
+  listExpenses(): void {
+    this._expenseService.getExpenses().subscribe(data=>this.expenses=this.filterExpenses(data))
+  }
 
+  filterExpenses(data:Expense[]){
+    return data.filter((e)=>{
+      return e.expense.toLowerCase().includes(this.filters.keyword.toLowerCase())
+    })
+
+}
 }
