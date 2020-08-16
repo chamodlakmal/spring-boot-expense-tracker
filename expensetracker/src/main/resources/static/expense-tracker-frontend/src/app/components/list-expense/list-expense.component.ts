16555b733625cdb398852482dee0affd3121a5b8
chamod_lab <chamodlakmal97@gmail.com>
2020-08-16 20:17:56 +0530

    sort functionality added

--- expensetracker/src/main/resources/static/expense-tracker-frontend/src/app/components/list-expense/list-expense.component.ts
+++ expensetracker/src/main/resources/static/expense-tracker-frontend/src/app/components/list-expense/list-expense.component.ts
@@ -11,13 +11,14 @@ export class ListExpenseComponent implements OnInit {
 
   filters={
     keyword:'',
+    sortBy:'Name'
 
   }
 
   constructor(private _expenseService: ExpenseService ,) { }
 
   ngOnInit(): void {
-    this._expenseService.getExpenses().subscribe(data=>this.expenses=data)
+    this.listExpenses()
   }
 
   listExpenses(): void {
@@ -27,6 +28,12 @@ export class ListExpenseComponent implements OnInit {
   filterExpenses(data:Expense[]){
     return data.filter((e)=>{
       return e.expense.toLowerCase().includes(this.filters.keyword.toLowerCase())
+    }).sort((a,b)=>{
+      if(this.filters.sortBy==='Name'){
+        return a.expense.toLowerCase()<b.expense.toLowerCase()?-1:1;
+      }else if(this.filters.sortBy==='Amount'){
+        a.amount<b.amount?-1:1;
+      }
     })
 
 }
